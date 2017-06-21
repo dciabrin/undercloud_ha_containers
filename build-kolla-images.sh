df3ced99a3305dbfe43917abf8a538017a077280
Damien Ciabrini <dciabrin@redhat.com>
2017-06-21 03:48:26 -0400

    added cinder-volume image

--- build-kolla-images.sh
+++ build-kolla-images.sh
@@ -18,7 +18,7 @@ PULL_IMGS="glance-api keystone"
 for i in $PULL_IMGS; do sudo docker pull tripleoupstream/centos-binary-$i:$TAG; done
 for i in $PULL_IMGS; do sudo docker tag tripleoupstream/centos-binary-$i:$TAG 192.168.24.1:8787/tripleoupstream/centos-binary-$i:$TAG; done
 
-BUILD_IMGS="mariadb rabbitmq haproxy redis cinder-base cinder-api cinder-backup cinder-scheduler manila-base manila-api manila-data manila-scheduler manila-share"
+BUILD_IMGS="mariadb rabbitmq haproxy redis cinder-base cinder-api cinder-backup cinder-scheduler cinder-volume manila-base manila-api manila-data manila-scheduler manila-share"
 time sudo kolla-build  --config-file=kolla-build.conf --template-override tripleo_kolla_template_overrides.j2 --namespace tripleoupstream --registry 192.168.24.1:8787 $BUILD_IMGS
 for i in $BUILD_IMGS; do sudo docker tag 192.168.24.1:8787/tripleoupstream/centos-binary-$i:$KOLLA_TAG 192.168.24.1:8787/tripleoupstream/centos-binary-$i:$TAG; done
 
